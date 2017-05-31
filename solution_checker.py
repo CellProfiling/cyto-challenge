71cc1cdd03a6fc705957aa50b3d0c2662d06852c
Martin Hjelmare <marhje52@kth.se>
2017-05-31 18:04:37 +0200

    Fix PEP8 issues

--- solution_checker.py
+++ solution_checker.py
@@ -1,3 +1,4 @@
+"""Calculate score for answers."""
 import argparse
 import csv
 import sys
@@ -5,6 +6,7 @@ import collections
 
 
 def parseargs():
+    """Parse command line arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument('submitted_answer')
     parser.add_argument('solution_key')
@@ -16,10 +18,11 @@ def parseargs():
 
 
 def read_key_file(f_path):
+    """Read key file."""
     ids = {}
     try:
-        with open(f_path, 'r') as f:
-            for line in csv.reader(f):
+        with open(f_path, 'r') as fil:
+            for line in csv.reader(fil):
                 id_ = line[0].strip()
                 classes = [x.strip() for x in line[1:]]
                 ids[id_] = classes
@@ -38,12 +41,13 @@ def read_key_file(f_path):
 
 
 def calc_precision(submitted, solution):
+    """Calculate precision."""
     precision = collections.defaultdict(lambda: {'tp': 0, 'fp': 0})
     for sub in submitted:
         sub_key = submitted[sub]
         try:
             sol_key = solution[sub]
-        except:
+        except IndexError:
             print(sub + ' Could not be found in solution key', file=sys.stderr)
 
         for key in sub_key:
@@ -52,19 +56,20 @@ def calc_precision(submitted, solution):
             else:
                 precision[key]['fp'] += 1
     for key in precision:
-        tp = precision[key]['tp']
-        fp = precision[key]['fp']
-        precision[key]['prec'] = tp/(tp + fp)
+        tp_prec = precision[key]['tp']
+        fp_prec = precision[key]['fp']
+        precision[key]['prec'] = tp_prec / (tp_prec + fp_prec)
     return dict(precision)
 
 
 def calc_recall(submitted, solution):
+    """Calculate recall."""
     recall = collections.defaultdict(lambda: {'tp': 0, 'fn': 0})
     for sub in submitted:
         sub_key = submitted[sub]
         try:
             sol_key = solution[sub]
-        except:
+        except IndexError:
             print(sub + ' Could not be found in solution key', file=sys.stderr)
 
         for key in sol_key:
@@ -74,13 +79,14 @@ def calc_recall(submitted, solution):
                 recall[key]['fn'] += 1
 
     for key in recall:
-        tp = recall[key]['tp']
-        fn = recall[key]['fn']
-        recall[key]['rec'] = tp/(tp + fn)
+        tp_recall = recall[key]['tp']
+        fn_recall = recall[key]['fn']
+        recall[key]['rec'] = tp_recall / (tp_recall + fn_recall)
     return dict(recall)
 
 
 def calc_f1_score(precision, recall):
+    """Calculate f1 score."""
     f1_score = collections.defaultdict(float)
     for key in precision:
         prec = precision[key]['prec']
@@ -93,6 +99,7 @@ def calc_f1_score(precision, recall):
 
 
 def main():
+    """Calculate score for answers."""
     submitted_answer, solution_key, include_classes = parseargs()
     submitted = read_key_file(submitted_answer)
     solution = read_key_file(solution_key)
@@ -100,14 +107,14 @@ def main():
     if len(submitted) != len(solution):
         print('Differring number of answers and solutions', file=sys.stderr)
         print('Num answers: {}, Num solutions: {}'.format(
-              len(submitted), len(solution), file=sys.stderr))
+            len(submitted), len(solution), file=sys.stderr))
 
     precision = calc_precision(submitted, solution)
     recall = calc_recall(submitted, solution)
     f1_score = calc_f1_score(precision, recall)
-    fs = 0.0
-    rs = 0.0
-    ps = 0.0
+    fin_f_score = 0.0
+    fin_r_score = 0.0
+    fin_p_score = 0.0
     if include_classes:
         keys = include_classes
     else:
@@ -118,22 +125,22 @@ def main():
             print(key, 'not in the available keys', file=sys.stderr)
             sys.exit(-1)
 
-        fs += f1_score[key]
+        fin_f_score += f1_score[key]
         try:
-            rs += recall[key]['rec']
-        except:
+            fin_r_score += recall[key]['rec']
+        except IndexError:
             pass
         try:
-            ps += precision[key]['prec']
-        except:
+            fin_p_score += precision[key]['prec']
+        except IndexError:
             pass
-    fs /= len(f1_score)
-    rs /= len(f1_score)
-    ps /= len(f1_score)
+    fin_f_score /= len(f1_score)
+    fin_r_score /= len(f1_score)
+    fin_p_score /= len(f1_score)
 
-    print('Recall:', rs)
-    print('Precision:', ps)
-    print('F1 score:', fs)
+    print('Recall:', fin_r_score)
+    print('Precision:', fin_p_score)
+    print('F1 score:', fin_f_score)
 
 
 if __name__ == '__main__':
