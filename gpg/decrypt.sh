d947ea909c8ed0b9bd0127e0a9f7d11c5a7ec807
Martin Hjelmare <marhje52@kth.se>
2017-06-01 14:29:43 +0200

    Update pgp public key
    * Update encrypt and decrypt scripts.
    * Encrypt csv files with new key.

--- gpg/decrypt.sh
+++ gpg/decrypt.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 PASSWORD=$1
-for FILE in *.*.gpg; do
-    echo "Extracting $FILE to ${FILE%.gpg}."
-    gpg --passphrase $PASSWORD --batch -d -q --yes \
+cd "$(dirname "$0")"
+cd "../"
+files=($(find -name "*" \( -name "*.gpg" \)))
+
+for FILE in ${files[*]}; do
+    echo "Extracting $FILE to ${FILE%.gpg}"
+    gpg --passphrase $PASSWORD --batch -d -q --no-tty --yes \
       --output "${FILE%.gpg}" "$FILE"
 done
