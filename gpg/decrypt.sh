e29e1e6903b58aaf56fb9f7df0128e2523915655
Martin Hjelmare <marhje52@kth.se>
2017-06-03 02:21:37 +0200

    Try to fix gpg decryption

--- gpg/decrypt.sh
+++ gpg/decrypt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-PASSWORD=$1
+#PASSWORD=$1
 cd "$(dirname "$0")"
 cd "../"
 files=($(find -name "*" \( -name "*.gpg" \)))
 
 for FILE in ${files[*]}; do
     echo "Extracting $FILE to ${FILE%.gpg}"
-    gpg --passphrase-file $PASSWORD --batch --no-tty -q --yes -d \
+    echo $gpg_password | gpg --passphrase-fd 0 --batch --no-tty -q --yes -d \
       -o "${FILE%.gpg}" "$FILE"
 done
