bd3793133bf7622a0a38fd23f8145e4b1b8d7f8e
Casper Winsnes <cwinsnes92@gmail.com>
2017-06-05 12:16:34 +0200

    scoring for challenge 4

--- challenge_4_checker.py
+++ challenge_4_checker.py
@@ -1,8 +1,15 @@
 from collections import defaultdict
-from solution_checker import read_key_file
+from solution_checker import read_key_file, calc_precision, calc_recall, score, calc_f1_score
 import argparse
 import csv
 import sys
+
+
+PREC = 'prec'
+REC = 'rec'
+TRUE_POS = 'tp'
+FALSE_POS = 'fp'
+FALSE_NEG = 'fn'
 
 
 def parseargs():
@@ -81,12 +88,49 @@ def translate_unknowns(submitted, novel, known_classes):
                 sub_classes[i] = submitted_translation[sub_classes[i]]
     return submitted
 
+
+def challenge4_score(f1_score, precision, recall):
+    fin_f_score = 0.0
+    fin_r_score = 0.0
+    fin_p_score = 0.0
+
+    for key in f1_score:
+        if key not in f1_score:
+            fin_f_score = 0
+        else:
+            fin_f_score += f1_score[key]
+        try:
+            fin_r_score += recall[key][REC]
+        except IndexError:
+            pass
+        try:
+            fin_p_score += precision[key][PREC]
+        except IndexError:
+            pass
+    fin_f_score /= len(f1_score)
+    fin_r_score /= len(f1_score)
+    fin_p_score /= len(f1_score)
+
+    print('Recall:', fin_r_score)
+    print('Precision:', fin_p_score)
+    print('F1 score:', fin_f_score)
+    return fin_r_score, fin_p_score, fin_f_score
+
 def main():
     submitted_answer, solution_key, novel_classes = parseargs()
     known_classes = get_known_classes(solution_key, novel_classes)
     novel = get_novel_ids(solution_key, novel_classes)
     submitted = read_key_file(submitted_answer)
+    solution = read_key_file(solution_key)
     translated_submitted = translate_unknowns(submitted, novel, known_classes)
+
+    # From here, most of the code for rare classes can be used
+    precision = calc_precision(translated_submitted, solution)
+    recall = calc_recall(translated_submitted, solution)
+    f1_score = calc_f1_score(precision, recall)
+    print(f1_score.keys())
+    print(translated_submitted)
+    challenge4_score(f1_score, precision, recall)
 
 
 if __name__ == '__main__':
