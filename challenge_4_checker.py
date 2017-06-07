4fbab3788df01efb2c07dfd7d41e361ecd987ba5
Martin Hjelmare <marhje52@kth.se>
2017-06-07 17:45:31 +0200

    Clean up

--- challenge_4_checker.py
+++ challenge_4_checker.py
@@ -60,29 +60,29 @@ def translate_unknowns(submitted, novel, known_classes):
         submitted_classes = set(submitted_classes) - known_classes
         solution_classes = novel[id_] - known_classes
 
-        for c in solution_classes:
-            for d in submitted_classes:
-                submitted_to_novel[d][c] += 1
+        for sol_cls in solution_classes:
+            for sub_cls in submitted_classes:
+                submitted_to_novel[sub_cls][sol_cls] += 1
 
     # I am aware that this is fugly, but it works
     # translates the classes submitted by the competitor into the novel class
     # that is present the most times for that unknown class.
     # If for example the class UC1 has been applied to the class A twice and
     # the class B once, UC1 will be translated to A in every instance.
-    for d in submitted_to_novel:
+    for sub_cls in submitted_to_novel:
         max_ = -1
         current = ''
-        for c in submitted_to_novel[d]:
-            if submitted_to_novel[d][c] > max_:
-                max_ = submitted_to_novel[d][c]
-                current = c
+        for cls_ in submitted_to_novel[sub_cls]:
+            if submitted_to_novel[sub_cls][cls_] > max_:
+                max_ = submitted_to_novel[sub_cls][cls_]
+                current = cls_
         occupied.add(current)
-        submitted_translation[d] = current
+        submitted_translation[sub_cls] = current
 
     for sub_classes in submitted.values():
-        for i, c in enumerate(sub_classes):
-            if c in submitted_translation:
-                sub_classes[i] = submitted_translation[c]
+        for i, cls_ in enumerate(sub_classes):
+            if cls_ in submitted_translation:
+                sub_classes[i] = submitted_translation[cls_]
 
     return submitted
 
@@ -99,7 +99,6 @@ def score_4(submitted_answer, solution_key, novel_classes):
     novel_classes = read_key_file(novel_classes)
     novel_classes = [
         class_ for row in novel_classes.values() for class_ in row]
-    print(novel_classes)
 
     known_classes = get_known_classes(solution, novel_classes)
     novel = get_novel_ids(solution, novel_classes)
